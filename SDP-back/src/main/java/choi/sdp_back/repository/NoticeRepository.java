c731c0e68f65afacdcb5ed69eaf0ec1c29e0f1be
Song <sjhuk25@gmail.com>
2025-12-29 22:12:11 +0900

    DDDDD

--- SDP-back/src/main/java/choi/sdp_back/repository/NoticeRepository.java
+++
@@ -1,7 +0,0 @@
-package choi.sdp_back.repository;
-
-import choi.sdp_back.entity.Notice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
-}
