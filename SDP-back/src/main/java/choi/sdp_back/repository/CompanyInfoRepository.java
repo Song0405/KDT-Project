286a90977af5ce2c98532a7499d12f09b115a3b7
Song0405 <148070272+Song0405@users.noreply.github.com>
2025-12-29 21:39:44 +0900

    Delete SDP-back directory

--- SDP-back/src/main/java/choi/sdp_back/repository/CompanyInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package choi.sdp_back.repository;
-
-import choi.sdp_back.entity.CompanyInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyInfoRepository extends JpaRepository<CompanyInfo, Long> {
-    CompanyInfo findFirstByOrderByIdAsc();
-}
--- SDP-back/src/main/java/choi/sdp_back/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package choi.sdp_back.repository;
-
-import choi.sdp_back.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
